2b8b92c50be77be7e3bf4bcd2b22cbf1468ac711
Santiago Donoso <sand@kea.dk>
2022-03-01 11:22:25 +0100

    ...

--- users_get.py
+++ users_get.py
@@ -1,7 +1,16 @@
 import sqlite3
 import json
 
+##############################
+def dict_factory(cursor, row):
+  d = {}
+  for idx, col in enumerate(cursor.description):
+    d[col[0]] = row[idx]
+  return d
+
+##############################
 db = sqlite3.connect("database.sqlite")
+db.row_factory = dict_factory
 users = json.dumps(db.execute("SELECT * FROM users").fetchall())
 print(users)
 
