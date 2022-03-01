62e0aec2633a6684de322289cdd7838d3caa3642
Santiago Donoso <sand@kea.dk>
2022-03-01 11:16:02 +0100

    ...

--- users_update.py
+++ users_update.py
@@ -1,11 +1,24 @@
 import sqlite3
 db = sqlite3.connect("database.sqlite")
+
+user = {
+  "user_id":"43cd555e-d475-4763-aeaa-788ae58f1996",
+  "user_name":"NEW USER NAME"
+}
+
 # UPDATE SET WHERE
 counter = db.execute("""
   UPDATE users
-  SET user_name = ?
-  WHERE user_id = ?
-""", ("NEW USER NAME", "43cd555e-d475-4763-aeaa-788ae58f1996")).rowcount
+  SET user_name = :user_name
+  WHERE user_id = :user_id
+""", user).rowcount
+
+
+# counter = db.execute("""
+#   UPDATE users
+#   SET user_name = ?
+#   WHERE user_id = ?
+# """, ("NEW USER NAME", "43cd555e-d475-4763-aeaa-788ae58f1996")).rowcount
 db.commit()
 print(f"Number of users updated: {counter}")
 
