96ca14f7a89c54db0b5bbad8597020111f708419
Santiago Donoso <sand@kea.dk>
2022-03-01 11:31:56 +0100

    ...

--- create-table-users.sql
+++ create-table-users.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
   user_id     TEXT UNIQUE NOT NULL,
   user_name   TEXT NOT NULL,
+  user_email  TEXT UNIQUE NOT NULL,
   PRIMARY KEY(user_id)
 ) WITHOUT ROWID;
--- users_post.py
+++ users_post.py
@@ -3,15 +3,17 @@ import uuid
 
 user_id = str(uuid.uuid4())
 user_name = "b"
+user_email = "@a"
 
 user = {
   "user_id" : user_id,
-  "user_name" : user_name
+  "user_name" : user_name,
+  "user_email" : user_email
 }
 
 db = sqlite3.connect("database.sqlite")
 # Using a dictionary and named place holders
-db.execute("INSERT INTO users VALUES(:user_id, :user_name)", user)
+db.execute("INSERT INTO users VALUES(:user_id, :user_name, :user_email)", user)
 
 
 # Placeholders with ? marks
